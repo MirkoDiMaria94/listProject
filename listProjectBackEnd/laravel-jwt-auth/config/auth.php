c03fda98060fa08e61623a9209c62a0f7c56b144
MirkoDiMaria94 <fanta.mirko90@gmail.com>
2022-05-20 17:43:15 +0200

    aggiornamento auth.php

--- listProjectBackEnd/laravel-jwt-auth/config/auth.php
+++ listProjectBackEnd/laravel-jwt-auth/config/auth.php
@@ -1,4 +1,14 @@
+
+
+
+
+
+
 <?php
+
+
+
+
 return [
     'defaults' => [
         'guard' => 'api',
@@ -19,7 +29,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model'  =>  App\User::class,
+            'model'  =>  App\Models\User::class,
         ]
     ]
     ];
