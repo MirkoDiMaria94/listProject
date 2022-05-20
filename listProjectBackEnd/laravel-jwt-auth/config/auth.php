b3521a707cf1d7b921e7a95b24c40444594e9638
MirkoDiMaria94 <fanta.mirko90@gmail.com>
2022-05-20 16:32:58 +0200

    modifica backend

---
+++ listProjectBackEnd/laravel-jwt-auth/config/auth.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'defaults' => [
+        'guard' => 'api',
+        'passwords' => 'users',
+    ],
+
+    'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+            'hash' => false,
+        ],
+    ],
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model'  =>  App\User::class,
+        ]
+    ]
+    ];
